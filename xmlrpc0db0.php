3dc46417a4e4f303d6dc3c7e857085805b956a6e
alwiishihab_ <muhammadalwi.20072@mhs.unesa.ac.id>
2023-04-04 10:00:06 +0700

    gambar ga muncul

--- xmlrpc0db0.php
+++ xmlrpc0db0.php
@@ -2,13 +2,13 @@
 	<service>
 		<engineName>WordPress</engineName>
 		<engineLink>https://wordpress.org/</engineLink>
-		<homePageLink>http://cctvs.id.test</homePageLink>
+		<homePageLink>https://cctvs-id.pages.dev/</homePageLink>
 		<apis>
-			<api name="WordPress" blogID="1" preferred="true" apiLink="http://cctvs.id.test/xmlrpc.php" />
-			<api name="Movable Type" blogID="1" preferred="false" apiLink="http://cctvs.id.test/xmlrpc.php" />
-			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="http://cctvs.id.test/xmlrpc.php" />
-			<api name="Blogger" blogID="1" preferred="false" apiLink="http://cctvs.id.test/xmlrpc.php" />
-				<api name="WP-API" blogID="1" preferred="false" apiLink="http://cctvs.id.test/wp-json/" />
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://cctvs-id.pages.dev//xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://cctvs-id.pages.dev//xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://cctvs-id.pages.dev//xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://cctvs-id.pages.dev//xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://cctvs-id.pages.dev//wp-json/" />
 			</apis>
 	</service>
 </rsd>
